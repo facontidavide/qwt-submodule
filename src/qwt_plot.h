ec4878d230d5aecab7287b096e1af828a0c80a3a
facontidavide <davide.faconti@gmail.com>
2016-04-11 23:28:31 +0200

    chnage needed

--- src/qwt_plot.h
+++ src/qwt_plot.h
@@ -194,7 +194,7 @@ public:
     void setAxisFont( int axisId, const QFont &f );
     QFont axisFont( int axisId ) const;
 
-    void setAxisScale( int axisId, double min, double max, double step = 0 );
+    virtual void setAxisScale( int axisId, double min, double max, double step = 0 );
     void setAxisScaleDiv( int axisId, const QwtScaleDiv & );
     void setAxisScaleDraw( int axisId, QwtScaleDraw * );
 
